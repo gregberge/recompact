fb739e34c4a7fc84af2d06e8c22cb22fc750258c
Greg Bergé <berge.greg@gmail.com>
2017-04-25 22:52:06 +0200

    tests: complete shareObservable test

--- src/__tests__/utils/shareObservable.test.js
+++ src/__tests__/utils/shareObservable.test.js
@@ -45,4 +45,42 @@ describe('shareObservable', () => {
     subscription2.unsubscribe()
     expect(unsubscribe).toHaveBeenCalledTimes(1)
   })
+
+  it('should forward complete', () => {
+    let observer
+    const observable = createObservable((_observer) => {
+      observer = _observer
+      return { unsubscribe: jest.fn() }
+    })
+
+    const sharedObservable = shareObservable(observable)
+    const observer1 = { next: jest.fn(), complete: jest.fn(), error: jest.fn() }
+    const observer2 = { next: jest.fn(), complete: jest.fn(), error: jest.fn() }
+    sharedObservable.subscribe(observer1)
+    sharedObservable.subscribe(observer2)
+    observer.complete('done')
+    expect(observer1.complete).toHaveBeenCalledTimes(1)
+    expect(observer1.complete).toHaveBeenCalledWith('done')
+    expect(observer2.complete).toHaveBeenCalledTimes(1)
+    expect(observer2.complete).toHaveBeenCalledWith('done')
+  })
+
+  it('should forward error', () => {
+    let observer
+    const observable = createObservable((_observer) => {
+      observer = _observer
+      return { unsubscribe: jest.fn() }
+    })
+
+    const sharedObservable = shareObservable(observable)
+    const observer1 = { next: jest.fn(), complete: jest.fn(), error: jest.fn() }
+    const observer2 = { next: jest.fn(), complete: jest.fn(), error: jest.fn() }
+    sharedObservable.subscribe(observer1)
+    sharedObservable.subscribe(observer2)
+    observer.error('error')
+    expect(observer1.error).toHaveBeenCalledTimes(1)
+    expect(observer1.error).toHaveBeenCalledWith('error')
+    expect(observer2.error).toHaveBeenCalledTimes(1)
+    expect(observer2.error).toHaveBeenCalledWith('error')
+  })
 })
