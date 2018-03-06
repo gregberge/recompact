4d39016cd879ebe54f0c6e2cf045b66b33b334ab
Julian Rosse <julian@helixbass.net>
2018-03-05 23:34:34 -0500

    test fallback behavior

---
+++ src/__tests__/utils/WeakMap.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable global-require */
+import React from 'react'
+import { shallow } from 'enzyme'
+
+describe('withProps using NonTrackingDummyWeakMap fallback', () => {
+  let withProps
+  let compose
+  let Dummy
+  let globalWeakMap
+  beforeAll(() => {
+    globalWeakMap = global.WeakMap
+    delete global.WeakMap
+    ;({ withProps, compose } = require('../../'))
+    ;({ Dummy } = require('../utils'))
+  })
+
+  afterAll(() => {
+    global.WeakMap = globalWeakMap
+  })
+
+  it('should pass additional props to base component', () => {
+    const DoReMi = withProps({ si: 'do', la: 'fa' })(Dummy)
+    expect(DoReMi.displayName).toBe('withProps(Dummy)')
+
+    const dummy = shallow(<DoReMi />).find(Dummy)
+    expect(dummy.prop('si')).toBe('do')
+    expect(dummy.prop('la')).toBe('fa')
+  })
+
+  it('can coexist with other non-compacted hoc', () => {
+    const DoReMi = compose(withProps({ si: 'do' }), withProps({ la: 'fa' }))(
+      Dummy,
+    )
+    expect(DoReMi.displayName).toBe('withProps(withProps(Dummy))')
+
+    const dummy = shallow(<DoReMi />).find(Dummy)
+    expect(dummy.prop('si')).toBe('do')
+    expect(dummy.prop('la')).toBe('fa')
+  })
+})
--- src/utils/WeakMap.js
+++ src/utils/WeakMap.js
@@ -1,9 +1,16 @@
+/* eslint-disable class-methods-use-this */
 class NonTrackingDummyWeakMap {
-  get() {}
-  set() {}
+  get() {
+    return this
+  }
+  set() {
+    return this
+  }
   has() {
     return false
   }
 }
 
-export default WeakMap || NonTrackingDummyWeakMap
+export default (typeof WeakMap === 'undefined'
+  ? NonTrackingDummyWeakMap
+  : WeakMap)
